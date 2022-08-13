8582a607f761deaf7d310df1dd9585e9c830b798
Atendra Suryvanshi <suryavanshi.atendra15@gmail.com>
2022-08-13 23:40:55 +0530

    adding exception handling

--- src/main/java/com/signature/recipe/exceptions/SpringExceptionHandler.java
+++ src/main/java/com/signature/recipe/exceptions/SpringExceptionHandler.java
@@ -3,7 +3,14 @@ package com.signature.recipe.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.thymeleaf.exceptions.TemplateInputException;
+import reactor.core.publisher.Mono;
 
 
 @Slf4j
@@ -11,19 +18,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class SpringExceptionHandler {
 
-/*  @ResponseStatus(HttpStatus.NOT_FOUND)
-  @ExceptionHandler(NotFoundException.class)
-  public ModelAndView handleNotFoundException(NotFoundException ex) {
-    log.error("Handling not found exception");
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+  public Mono<String> handleNotFoundException(Model model, Exception ex) {
+    log.error("Handling not found or null exception");
     log.error("Message : {}", ex.getMessage());
-    return new ModelAndView("404").addObject("ex", ex);
+    model.addAttribute("ex", ex);
+    return Mono.just("404");
   }
 
   @ResponseStatus(HttpStatus.BAD_REQUEST)
-  @ExceptionHandler(NumberFormatException.class)
-  public ModelAndView handlerNumberFormatException(NumberFormatException ex) {
-    log.error("Handling number format exception");
+  @ExceptionHandler({NumberFormatException.class, WebExchangeBindException.class})
+  public Mono<String> handlerNumberFormatException(Model model, Exception ex) {
+    log.error("Handling data binding exception");
     log.error("Message : {}", ex.getMessage());
-    return new ModelAndView("400").addObject("ex", ex);
-  }*/
+    model.addAttribute("ex", ex);
+    return Mono.just("400");
+  }
 }
